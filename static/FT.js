12d3e640878ec4a1c3601058a9376ce1cdee85be
weia88 <weia88@uw.edu>
2021-01-28 19:31:56 -0800

    Access and display of Json content!

--- static/FT.js
+++ static/FT.js
@@ -1,21 +1,24 @@
 "use strict";
 
-function updateTracker(){
+// update the main food tracker --> submitting a new entry
+function AddEntry(){
     var food = document.getElementById("food_item").value;
     var date = document.getElementById("date").value;
     var time = document.getElementById("time_of_day").value;
     var calories = document.getElementById("calories").value;
-    console.log(food + date + time + calories);
     
-    var check = isFormComplete(food, date, time, calories);
-    console.log(check);
-    if (check){
-        resetForm();
-    }
+    // var check = isFormComplete(food, date, time, calories);
+    // if (check){
+    //     ResetForm();
+    // }
+
+    // Check if existing json file exists, if does then update and write to that
+    // Else, create json and write to that
+
     
 }
 
-function resetForm(){
+function ResetForm(){
     // document.getElementById("form1").reset(); this shit shouldn't be broken but it is
     document.getElementById("food_item").value = "";
     document.getElementById("date").value = "";
@@ -58,3 +61,71 @@ function isFormComplete(food, date, time, calories){
     return true; 
 }
 
+// Reads the Json file in explorer 
+function ReadJsonFile(){
+    var url = "/Example.json";
+    var file_content;
+    $.ajax({
+        type:"GET",
+        url: url,
+        dataType:"json",
+        success: function(data) {
+            var temp = JSON.stringify(data);
+            var parsedJSON = JSON.parse(temp);
+            file_content = parsedJSON;
+        },
+        async: false  
+    });
+    return file_content;
+    
+    // var test;
+    //     $.getJSON("/Example.json", function(data){
+    //         var temp = JSON.stringify(data);
+    //         var parsedJSON = JSON.parse(temp);
+    //         test = parsedJSON;
+    //     });
+
+
+
+
+    //     var dict = {};
+//     var jsonFile = $.getJSON("/Example.json", function(data) {
+//         $.each(data, function(key, value){
+//             var k = key;
+//             dict["k"] = [];
+//             $.each(value, function(index, value){
+//                 $.each(value, function(key, value){
+//                     console.log(key + "---" + value);
+//                     dict["k"].push  
+//                 });
+//             });  
+//         });
+//     });
+}
+
+function WriteJsonFile(x, y, z, a){
+    
+}
+
+// Displays the content of the JsonFile into the wrapper container
+function DisplayJsonFile(input_list){
+    var wrapper = $("#wrapper"), container;
+    for (var key in input_list){
+        container = $('<div id="list_displayed" class="container"></div>');
+        wrapper.append(container);
+        container.append('<div class="date">' + key + '</div>');
+        for (const dict of input_list[key]){
+            container.append('<div class="name">' + dict.name + '</div>');
+            container.append('<div class="time">' + dict.time + '</div>');
+            container.append('<div class="calories">' + dict.calories + " Cals" + '</div>');
+
+        }
+    }
+}
+
+// A function temporary used to call functions
+function throwaway(){
+    var temp = ReadJsonFile();
+    console.log(temp);
+    DisplayJsonFile(temp);
+}
